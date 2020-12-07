cb3b1dbd17bb9d11dd006cdbc4159a4f5e5b56a0
nickdeans <nickabramowicz@gmail.com>
2020-12-07 15:56:15 -0800

    test of deployment

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AboutMe
-Lab 02: jQuery Selectors and Events
+Lab 06: Node, npm, and Express
 
 ## Authors/Contributors
 Nick Abramowicz
@@ -11,4 +11,10 @@ I want to be able to filter the images so that I can view only images that match
 JavaScript
 JSON
 
-# Time Estimate
+## Time Estimate
+### Lab Day 1
+#### Feature 1
+- Number and name of feature: Feature 1
+- Estimate of time needed to complete: 1 Hour
+- Start time: 2:41
+- Finish time: 4:28
--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 'use strict';
 
+// ======= Server Start ==========
+
 const express = require('express');
 const cors = require('cors');
+const { static } = require('express');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// ======== Middleware =========
+
 app.use(cors());
+
+// ======== Routes ==========
+
+app.get('/location', function(req,res){
+    res.send('./index.html');
+})
+
+// ========== Callback FUnctions =============
+
+app.use(express.static('./public'));
+app.listen(PORT, () => console.log('server is working on'));
